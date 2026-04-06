959b05c031e10038c50c5ae22bc1fa494dcb655b
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-04-06 14:49:20 +0200

    fmt

--- src/onnx.rs
+++ src/onnx.rs
@@ -1,6 +1,6 @@
 use crate::ClipError;
 use ort::ep::ExecutionProviderDispatch;
-use ort::session::{builder::GraphOptimizationLevel, Session};
+use ort::session::{Session, builder::GraphOptimizationLevel};
 use std::path::Path;
 use std::sync::RwLock;
 
